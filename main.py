37cfd0168bfa5c1f8abba110d157595d42f709d9
Silent <hamidreza.neo@gmail.com>
2021-07-25 18:20:59 +0430

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ print(f"{DataBase.nCount()}/{DataBase.Count()} Users\n")
 print("1- Get guilds")
 print("2- Fetch users from guilds")
 print("3- Send message to users")
-print("4- Reset sent data")
-print("5- Truncate users")
+print("4- Send message with friend request")
+print("5- Reset sent data")
+print("6- Truncate users")
 
 choice = int(input("Choose by number: "))
 
@@ -31,8 +32,10 @@ elif choice == 3:
 			elif not int(user[3]):
 				print(f"Sending to {user[1]} "+"\033[31m"+"Failed"+"\033[0m")
 elif choice == 4:
+	...
+elif choice == 5:
 	print("Reseting...")
 	DataBase.Reset()
-elif choice == 5:
+elif choice == 6:
 	print("Truncating...")
 	DataBase.truncate()
